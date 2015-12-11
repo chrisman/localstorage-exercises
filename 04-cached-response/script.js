36fe9b823296f9227c457aadba053650a00fa121
Christopher Brown <christopher.p.brown@gmail.com>
2015-12-11 10:16:56 -0700

    :saxaphone: localstorage!

--- 04-cached-response/script.js
+++ 04-cached-response/script.js
@@ -1,7 +1,11 @@
 $.get('https://www.reddit.com/r/aww.json')
   .done(function (response) {
-    //What to do if HTTP was successful
+    var redditData = response
+    localStorage.setItem('redditData', JSON.stringify(redditData))
+    console.log('stored');
   })
   .fail(function (error) {
     //What to do if HTTP failed
+    console.error("Error: " + error)
+    return cachedData = JSON.parse(localStorage.getItem('redditData'))
   })
